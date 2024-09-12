450838b706cd67f1398a6c617bbe68c47a34c5de
Your Name <Raditya-Z>
2024-09-12 18:54:41 +0700

    Data mahasiswa

--- src/Pertemuan2/P2_Percobaan3_pertanyaan1_23.java
+++ src/Pertemuan2/P2_Percobaan3_pertanyaan1_23.java
@@ -2,33 +2,17 @@ package Pertemuan2;
 
 public class P2_Percobaan3_pertanyaan1_23 {
     public static void main(String[] args) {
-        System.out.println("                               KARTU RENCANA STUDI (KRS)                               ");
-        System.out.println("");
-        System.out.println("NIM             :244107020144");
-        System.out.println("NAMA            :RADITYA ZANDRA FADHILLAH");
-        System.out.println("PROGAM STUDI    :Diploma IV Teknik Informatika");
-        System.out.println("KELAS           :1G");
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("|   KODE MK   |                  MATA KULIAH                  |    SKS    |    JAM    |");
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("|  RT1241001  |Pancasila                                      |     2     |     2     |");
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("|  RT1241001  |Konsep Teknologi Informasi                     |     2     |     2     |");
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("|  RT1241001  |Critical Thinking dan Problem Solving          |     2     |     2     |");
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("|  RT1241001  |Matematika Dasar                               |     2     |     2     |");
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("|  RT1241001  |Rekayasa Perangkat Lunak                       |     2     |     2     |");
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("|  RT1241001  |Dasar Pemograman                               |     2     |     2     |");
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("|  RT1241001  |Praktikum Dasar Pemograman                     |     2     |     2     |");
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("|  RT1241001  |Keselamatan dan Kesehatan Kerja                |     2     |     2     |");
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("|             |                    JUMLAH                     |     18    |     34    |");
-        System.out.println("---------------------------------------------------------------------------------------"); 
-        
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama    : Raditya Zandra Fadhillah");
+        System.out.println("NIM     : 244107020144");
+        System.out.println("Kelas   : 1-G");
+        System.out.println("No Absen: 23");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : Jalan Peltu Sujono Gang Cilung No 35");
+        System.out.println("No HP   : 081232168993");
+        System.out.println("Hobi    : Mendegarkan Musik");
+    }
     }
 }
