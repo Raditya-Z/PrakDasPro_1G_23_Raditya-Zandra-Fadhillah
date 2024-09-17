2bf0d002e52f7172628599cd48f546b6d7183c60
Your Name <Raditya-Z>
2024-09-17 19:31:05 +0700

    P3_Percobaan1_PertanyaanNo5

--- src/Pertemuan3/Siakad23.java
+++ src/Pertemuan3/Siakad23.java
@@ -4,17 +4,16 @@ public class Siakad23 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String nama, nim;
-        char kelas;
+        String nama, nim, kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUjian, Nilai-Akhir;
         
         System.out.print("Masukkan nama: ");
         nama = sc.nextLine();
         System.out.print("Masukkan NIM: ");
         nim = sc.nextLine();
         System.out.print("Masukkan kelas: ");
-        kelas = sc.nextLine().charAt(0);
+        kelas = sc.nextLine();
         System.out.print("Masukkan nomor absen: ");
         absen = sc.nextByte();
 
@@ -25,11 +24,11 @@ public class Siakad23 {
         System.out.print("Masukkan nilai ujian: ");
         nilaiUjian = sc.nextDouble();
 
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        Nilai-Akhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
 
         System.out.println("Nama: " + nama + " NIM: " + nim);
         System.out.println("Kelas: " + kelas + " Absen: " + absen);
-        System.out.println("Nilai Akhir: " + nilaiAkhir);
+        System.out.println("Nilai Akhir: " + Nilai-Akhir);
 
     }
 }
