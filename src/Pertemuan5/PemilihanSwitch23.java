7cf9e3ea94385655fc3944ba555e79c5338481af
Your Name <Raditya-Z>
2024-10-02 18:23:17 +0700

    P5_Percobaan2_PertanyaanNo3

--- src/Pertemuan5/PemilihanSwitch23.java
+++ src/Pertemuan5/PemilihanSwitch23.java
@@ -26,7 +26,8 @@ public class PemilihanSwitch23 {
             case '/':
                 hasil = angka1 / angka2;
                 break;
-
+            default:
+                System.out.println("operator yang dipilih tidak sesuai");
         }
 
         System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
