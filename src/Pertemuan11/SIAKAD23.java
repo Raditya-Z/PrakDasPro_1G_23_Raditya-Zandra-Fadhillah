4e06d4db37641f692efdb94809644150d6c60609
Your Name <Raditya-Z>
2024-11-14 22:47:10 +0700

    P11_percobaan4_PertanyaanNo2

--- src/Pertemuan11/SIAKAD23.java
+++ src/Pertemuan11/SIAKAD23.java
@@ -1,12 +1,15 @@
 package Pertemuan11;
-
 import java.util.Scanner;
-
 public class SIAKAD23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int mahasiswa, matkul;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        mahasiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah matkul : ");
+        matkul = sc.nextInt();
 
-        int[][] nilai = new int[4][3];
+        int[][] nilai = new int[mahasiswa][matkul];
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
@@ -17,20 +20,20 @@ public class SIAKAD23 {
                 totalPersiwa += nilai[i][j];
             }
 
-            System.out.println("Nilai rata-rata: " + totalPersiwa/3);
+            System.out.println("Nilai rata-rata: " + totalPersiwa/matkul);
         }
 
         System.out.println("\n====================================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah:");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < matkul; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < mahasiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
 
-            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul /4);
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + totalPerMatkul / mahasiswa);
         }
     }
 }
