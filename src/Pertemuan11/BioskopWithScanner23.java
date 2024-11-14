2b8853ae16b86335699740717910340e5e1f6ab0
Your Name <Raditya-Z>
2024-11-14 17:38:04 +0700

    P11_percobaan2_PertayaanNo2

--- src/Pertemuan11/BioskopWithScanner23.java
+++ src/Pertemuan11/BioskopWithScanner23.java
@@ -4,27 +4,52 @@ public class BioskopWithScanner23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int baris, kolom;
+        int baris, kolom, opsi;
         String nama, next;
 
         String[][] penonton = new String[4][2];
 
+        
+
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+            System.out.println("----------------------------");
+            System.out.println("======== OPSI MENU =========");
+            System.out.println("----------------------------");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.println("----------------------------");
+            System.out.print("Pilih opsi (1-3) : ");
+            opsi = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
-
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
-
-            if (next.equalsIgnoreCase("n")) {
+            if (opsi == 1) {
+                while (true) {
+                    System.out.print("Masukkan nama: ");
+                nama = sc.nextLine();
+                System.out.print("Masukkan baris: ");
+                baris = sc.nextInt();
+                System.out.print("Masukkan kolom: ");
+                kolom = sc.nextInt();
+                sc.nextLine();
+                penonton[baris-1][kolom-1] = nama;
+                System.out.print("Input penonton lainnya? (y/n): ");
+                next = sc.nextLine();
+    
+                if (next.equalsIgnoreCase("n")) {
                 break; 
+                }
+                }
+                    
+            } else if (opsi == 2) {
+                System.out.println("======= DAFTAR PENONTON =======");
+                for (int i = 0; i < penonton.length; i++) {
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        System.out.println("Penonton baris" + i + ", " + "kolom" + j + " = " + penonton[i][j]);
+                    }
+                }
+            } else if (opsi == 3) {
+                break;
             }
 
         }
