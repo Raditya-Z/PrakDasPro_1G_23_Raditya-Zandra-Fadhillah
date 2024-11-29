147463440de56073fcec9aa012484591f6364609
Your Name <Raditya-Z>
2024-11-29 14:57:21 +0700

    P13_percobaan3

--- src/Pertemuan13/Kafe23.java
+++ src/Pertemuan13/Kafe23.java
@@ -1,4 +1,5 @@
 package Pertemuan13;
+import java.util.Scanner;
 public class Kafe23 {
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {  
         System.out.println("Selamat datang, " + namaPelanggan + "!");  
@@ -26,8 +27,23 @@ public class Kafe23 {
         }
 
     }
+
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = {15000, 20000, 22000, 12000, 10000, 18000};
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+    }
     public static void main(String[] args) {
-        Menu("Andi", true, "DISKON30");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
     }
 
 }
