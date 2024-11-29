929da124da76aae5f29c2896513147472b58c5b5
Your Name <Raditya-Z>
2024-11-29 14:45:33 +0700

    P13_percobaan2_PertanyaanNo5

--- src/Pertemuan13/Kafe23.java
+++ src/Pertemuan13/Kafe23.java
@@ -1,6 +1,6 @@
 package Pertemuan13;
 public class Kafe23 {
-    public static void Menu(String namaPelanggan, boolean isMember) {  
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {  
         System.out.println("Selamat datang, " + namaPelanggan + "!");  
         
         if (isMember) {  
@@ -15,11 +15,19 @@ public class Kafe23 {
         System.out.println("5. Roti Bakar - Rp 10.000");  
         System.out.println("6. Mie Goreng - Rp 18.000");  
         System.out.println("===========================");  
-        System.out.println("Silakan pilih menu yang Anda inginkan.");  
+        System.out.println("Silakan pilih menu yang Anda inginkan.");
+
+        if (kodePromo == "DISKON50") {
+            System.out.println("diskon 50%");
+        } else if (kodePromo == "DISKON30") {
+            System.out.println("diskon 30%");
+        } else {
+            System.out.println("Kode Invalid");
+        }
+
+    }
+    public static void main(String[] args) {
+        Menu("Andi", true, "DISKON30");
     }
 
-    public static void main(String[] args) {
-        Menu("Andi", true);
-    }
-    
 }
