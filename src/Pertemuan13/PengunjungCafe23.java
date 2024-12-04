5ae28f6fabe34bd14b4652f0d36a9d63413b576a
Your Name <Raditya-Z>
2024-12-04 13:30:08 +0700

    P13_percobaan4_PertanyaanNo2

--- src/Pertemuan13/PengunjungCafe23.java
+++ src/Pertemuan13/PengunjungCafe23.java
@@ -3,8 +3,8 @@ package Pertemuan13;
 public class PengunjungCafe23 {
     public static void daftarPengunjung(String... namaPengujung) {
         System.out.println("Daftar Nama Pengunjung:");
-        for (int i = 0; i < namaPengujung.length; i++) {
-            System.out.println("- " + namaPengujung[i]);
+        for (String daftarPengunjung : namaPengujung) {
+            System.out.println("- " + daftarPengunjung);
         }
     }
 
