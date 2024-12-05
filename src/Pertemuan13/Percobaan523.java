30436cc6efb3a8e6cb73fc6f384cc988d81819f7
Your Name <Raditya-Z>
2024-12-05 17:03:21 +0700

    P13_percobaan5

--- src/Pertemuan13/Percobaan523.java
+++ src/Pertemuan13/Percobaan523.java
@@ -1,13 +1,19 @@
 package Pertemuan13;
-
 import java.util.Scanner;
-
 public class Percobaan523 {
+    static int hitungLuas (int pjg, int lb) {
+        int Luas=pjg*lb;
+        return Luas;
+    }
+
+    static int hitungVolume (int tinggi, int a, int b) {
+        int volume= hitungLuas(a, b)*tinggi;
+        return volume;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         int p, l, t, L, vol;
-
         System.out.println("Masukkan panjang");
         p=input.nextInt();
         System.out.println("Masukkan lebar");
@@ -15,10 +21,10 @@ public class Percobaan523 {
         System.out.println("Masukkan tinggi");
         t=input.nextInt();
 
-        L=p*l;
+        L=hitungLuas(p, l);
         System.out.println("Luas Persegi panjang adalah "+L);
 
-        vol=p*l*t;
+        vol=hitungVolume(t, p, l);
         System.out.println("Volume balok adalah "+vol);
 
     }
