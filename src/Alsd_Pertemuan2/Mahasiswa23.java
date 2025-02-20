390d018563ddfcdbd2c046757b86f2b1a5402d83
Your Name <Raditya-Z>
2025-02-20 09:48:55 +0700

    Pertemuan2

---
+++ src/Alsd_Pertemuan2/Mahasiswa23.java
@@ -0,0 +1,39 @@
+package Alsd_Pertemuan2;
+public class Mahasiswa23 {
+        String nama;
+        String nim;
+        String kelas;
+        double ipk;
+
+    void tampilkanInformasi () {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("IPK: " + ipk);
+        System.out.println("Kelas: " + kelas);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (ipk < 0.0 || ipk > 4.0) {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        } else {
+            ipk = ipkBaru;
+        }
+        
+    }
+
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
