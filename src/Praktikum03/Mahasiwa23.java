7fa618221cc73c39b3d2487bfd57fd0bba7e18d6
Your Name <Raditya-Z>
2025-02-27 09:37:45 +0700

    Pertemuan3_Pertanyaan1_Percobaan2

--- src/Praktikum03/Mahasiwa23.java
+++ src/Praktikum03/Mahasiwa23.java
@@ -5,5 +5,18 @@ public class Mahasiwa23 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    void cetakInfo() {
+        for (int i = 0; i < 1; i++) {
+            System.out.println("NIM     : "+ nim);
+            System.out.println("Nama    : "+ nama);
+            System.out.println("Kelas   : "+ kelas);
+            System.out.println("IPK     : "+ ipk);
+            System.out.println("-----------------------------");
+        }
+    
+    }
     
 }
+
+
