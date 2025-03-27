44dfe6bfc66811e5203c995dc449ec6c294810ba
Your Name <Raditya-Z>
2025-03-27 13:32:43 +0700

    P7_Percobaan2

--- src/Praktikum06/MahasiswaBerprestasi22.java
+++ src/Praktikum06/MahasiswaBerprestasi22.java
@@ -83,5 +83,19 @@ public class MahasiswaBerprestasi22 {
             System.out.println("ipk\t: " + listMhs[pos].ipk);
         }
     }
-        
+    
+    int findBinarySearch (double cari, int left, int right){
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (cari == listMhs[mid].ipk) {
+                return (mid);
+            } else if (listMhs[mid].ipk > cari) {
+                return findBinarySearch(cari, left, mid - 1);
+            } else {
+                return findBinarySearch(cari, mid + 1, right);
+            }
+        }
+        return -1;
+    }
 }
--- src/Praktikum06/MahasiswaDemo22.java
+++ src/Praktikum06/MahasiswaDemo22.java
@@ -23,12 +23,20 @@ public class MahasiswaDemo22 {
 
         list.tampil();
         //melakukan pencarian data sequential
-        System.out.println("-----------------------------");
+        System.out.println("-----------------------------------------------");
         System.out.println("Pencarian Data");
-        System.out.println("-----------------------------");
+        System.out.println("-----------------------------------------------");
         System.out.println("masukkan ipk mahasiswa yang dicari: ");
         System.out.print("IPK: ");
         double cari = input.nextDouble();
+
+        System.out.println("-----------------------------");
+        System.out.println("menggunakan binary search");
+        System.out.println("-----------------------------");
+        double posisi2 = list.findBinarySearch(cari, 0, jumMhs-1);
+        int pss2 = (int) posisi2;
+        list.tampilPosisi(cari, pss2);
+        list.tampilDataSearch(cari, pss2);
 
         System.out.println("menggunakan sequential search");
         double posisi = list.sequentialSearching(cari);
