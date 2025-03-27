6c37cd4c593a7cc664dba5afc8963c34cd16d4b6
Your Name <Raditya-Z>
2025-03-27 13:53:02 +0700

    P7_Percobaan2_PertanyaanNo4

--- src/Praktikum06/MahasiswaBerprestasi22.java
+++ src/Praktikum06/MahasiswaBerprestasi22.java
@@ -90,7 +90,7 @@ public class MahasiswaBerprestasi22 {
             mid = (left + right) / 2;
             if (cari == listMhs[mid].ipk) {
                 return (mid);
-            } else if (listMhs[mid].ipk > cari) {
+            } else if (listMhs[mid].ipk < cari) {
                 return findBinarySearch(cari, left, mid - 1);
             } else {
                 return findBinarySearch(cari, mid + 1, right);
